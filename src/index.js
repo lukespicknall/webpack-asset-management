8d2da4d8fffd576bf048461f2892a15b182a0a34
Luke Spicknall <lukejspicknall@gmail.com>
2023-06-16 17:51:50 -0400

    install style-loader and css loader as devDependencies and add style.css file. add module to webpacks.config.js that has a rule to use style-loader and css-loader. import style.css into index. js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import _ from "lodash";
+import './style.css'
 
 function component() {
-  const element = document.createElement("div");
-
+  const element = document.createElement("h2");
 
   element.innerHTML = _.join(["Hello", "Ronnie"], " ");
+  element.classList.add('hello')
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,15 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   mode: "development",
 };
+
+
