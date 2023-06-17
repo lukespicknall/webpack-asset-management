68666d065bd9e24b1adcbe737805ba37687cb9ea
Luke Spicknall <lukejspicknall@gmail.com>
2023-06-17 14:40:19 -0400

    add webpack image asset handler to it's config file so that i can then import images into index.js ad append them to index.html. realizing the script tage in index.html oought to be inside <body>, not inside <head>. also connected icon.png to index.html through style.css. all bundled together in webpack.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from "lodash";
 import './style.css'
+import Icon from './icon.png'
 
 function component() {
   const element = document.createElement("h2");
 
   element.innerHTML = _.join(["Hello", "Ronnie"], " ");
   element.classList.add('hello')
+
+  const myIcon = new Image()
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon)
 
   return element;
 }
