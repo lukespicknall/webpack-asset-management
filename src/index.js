bf05f56205544cac79f68786c84af3c1ebefe16f
Luke Spicknall <lukejspicknall@gmail.com>
2023-06-17 21:18:22 -0400

    try to add font to webpack bundle, but cant get it to change on html. problem at the @font-face?

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import _ from "lodash";
-import './style.css'
-import Icon from './icon.png'
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
   const element = document.createElement("h2");
 
   element.innerHTML = _.join(["Hello", "Ronnie"], " ");
-  element.classList.add('hello')
+  element.classList.add('hello');
 
-  const myIcon = new Image()
+  const myIcon = new Image();
   myIcon.src = Icon;
 
-  element.appendChild(myIcon)
-
+  element.appendChild(myIcon);
   return element;
+
 }
 
 document.body.appendChild(component());
+
+
